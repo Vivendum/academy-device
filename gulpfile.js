75d2fb0e30806c41fae9627bc611563297e13b01
Vivendum <vivendum@mail.ru>
2020-07-07 21:18:38 +0500

    automation: setting rigger

--- gulpfile.js
+++ gulpfile.js
@@ -2,10 +2,12 @@
 
 var gulp = require("gulp");
 var plumber = require("gulp-plumber");
+var rigger = require("gulp-rigger");
 
 gulp.task("build-html", function() {
   return gulp.src("source/template/*.html")
     .pipe(plumber())
+    .pipe(rigger())
     .pipe(gulp.dest("build/before"))
     .pipe(gulp.dest("build/after"));
 });
